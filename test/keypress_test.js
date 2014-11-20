8ded2bcd0561769b9acbd8fbef471e2a8604006e
sgyyz <itnote.yyz@gamil.com>
2014-11-20 23:25:22 +0800

    add the integer delete event test

--- test/keypress_test.js
+++ test/keypress_test.js
@@ -26,3 +26,19 @@ test("delete the numbers", function() {
 
 	equal(input.val(), "0.12", "delete the last number.");
 });
+
+test("test delete when the number is integer.", function() {
+	var input = $("#input").formatmoney({
+		precision: 0 
+	});
+
+	keypress(input, 3);
+	keypress(input, 6);
+	keypress(input, 4);
+	keypress(input, 8);
+	keypress(input, 9);
+
+	keydown(input, "backspace");
+
+	equal(input.val(), "3,648", "delete the last number.");
+});
